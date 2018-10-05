bfeb77276cd2d9f03045eb21d00b6217f241cc52
amigcamel <amigcamel@gmail.com>
2018-10-06 02:16:19 +0800

    test critera

--- tests/test_json.py
+++ tests/test_json.py
@@ -3,8 +3,17 @@ import pytest
 
 from core.quiz import collect_quizzes
 
+quizzes = collect_quizzes()
 
-@pytest.mark.parametrize('Q', collect_quizzes())
+
+@pytest.mark.parametrize('Q', quizzes)
 def test_valid_quiz(Q):
     """Validate quiz."""
     assert Q()
+
+
+@pytest.mark.parametrize('Q', quizzes)
+def test_quiz_criteria(Q):
+    """Test if criteria are correct."""
+    status, _ = Q()('\n'.join(Q.test_code))
+    assert status == 1
