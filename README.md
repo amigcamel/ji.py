c449e6a594ce5c60c584ce540bdc5c027cd3a515
amigcamel <amigcamel@gmail.com>
2018-10-11 13:37:36 +0800

    version 0.0.1

--- README.md
+++ README.md
@@ -3,17 +3,33 @@
 吉.py
 
 ---
+吉.py 是一個幫助學習Python的小工具，  
+題目按照Python的基本資料型態作為分類，  
+使用者可以將不熟悉的題目收藏（點選右上角愛心圖示），  
+讓日後複習更加方便。
 
-## Installation
+如果妳/你覺得這個專案不錯，請在右上角幫我按顆星星，  
+或是一起加入開發的行列，謝謝！
+
+如果有任何建議或發現任何問題，歡迎在[issues](https://github.com/amigcamel/ji.py/issues)留下任何訊息。
+
+![screenshot](./screenshot.png)
+
+## 安裝 
 
     $ pip install -U ji.py
     $ ji.py
 
-Or, clone this repo and run:
+或是 clone 這份專案後，執行
 
     $ python -c 'from core.main import run_app; run_app()'
 
 
+## 反安裝
+
+    $ pip uninstall ji.py
+
 ## Test
 
     tox
+
--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 """Package setup."""
 from setuptools import setup, find_packages
 
+import core
+
+long_description = '吉.py 是一個幫助學習Python的小工具，題目按照Python的基本資\
+料型態作為分類，使用者可以將不熟悉的題目收藏（點選右上角愛心圖示）， 讓日後複\
+習更加方便。 如果妳/你覺得這個專案不錯，請在右上角幫我按顆星星，或是一起加入開\
+發的行列，謝謝！'
+
 
 setup(
     name='ji.py',
     author='amigcamel(阿吉/Aji)',
     author_email='amigcamel@gmail.com',
-    version='0.0.0',
+    version=core.__version__,
     url='https://github.com/amigcamel/ji.py',
     packages=find_packages(),
     description='吉.py 是一個幫助學習Python的小工具',
-    long_description='吉.py 是一個幫助學習Python的小工具',
+    long_description=long_description,
     install_requires=[],
     package_data={
         'core': ['data/*.json'],
