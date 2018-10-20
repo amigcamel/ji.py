676f46d39bc80a69168b8a5bb98cb558ca1a6444
amigcamel <amigcamel@gmail.com>
2018-10-20 14:06:08 +0800

    bump version to 0.0.1a6; finish #1

--- jipy/__init__.py
+++ jipy/__init__.py
@@ -2,7 +2,7 @@ from multiprocessing.managers import SyncManager
 
 from .multiprocessing_extras import SetProxy
 
-__version__ = '0.0.1a5'
+__version__ = '0.0.1a6'
 
 
 SyncManager.register('set', set, SetProxy)
